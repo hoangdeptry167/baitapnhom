c871f91b27b5216160ed4f00d741b1a0d3658e40
hoangdeptry167 <hoangdeptry167@gmail.com>
2020-08-22 16:44:58 +0700

    das

--- js/page_1.js
+++ js/page_1.js
@@ -1,6 +1,6 @@
 console.log('hello world');
 function Redirect() {
-   window.location="http://127.0.0.1:5501/html/page_2.html";
+   window.location="http://127.0.0.1:5502/html/page_2.html";
 }
 setTimeout('Redirect()', 5500);
 
--- test.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-    TweenMax.from($('.box'),2,{scale:2,opacity:0.5,delay:1});
-})
